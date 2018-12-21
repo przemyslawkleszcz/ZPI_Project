fd79691409fd00a36f16b310e7453cee0a0a2a4f
Przemyslaw Kleszcz <kleszcz@mail.com>
2018-12-21 14:36:04 +0100

    Ukrycie boardów nie przypisanych do danych użytkowników

--- src/app/kanban/kanban.component.ts
+++ src/app/kanban/kanban.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { map } from 'rxjs/operators';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-kanban',
@@ -11,14 +12,22 @@ export class KanbanComponent {
   public kanbanData: any;
   public boardName: string;
   public items: any;
+  private userUid: any;
 
   constructor(private db: AngularFirestore) {
     this.boardName = "Enter name of a new board";
-
+    this.userUid = firebase.auth().currentUser.uid;
     this.items = db.collection("Boards")
       .snapshotChanges()
       .pipe(map(actions => {
-        return actions.map(a => {
+        return actions.filter(a => {
+          const data = a.payload.doc.data();
+          let members = (<any>data).Members;
+          if (members.indexOf(this.userUid) < 0)
+            return false;
+          else
+            return true;
+        }).map(a => {
           const data = a.payload.doc.data();
           const id = a.payload.doc.id;
           return { id, data };
@@ -27,7 +36,7 @@ export class KanbanComponent {
   }
 
   addBoard(e) {
-    this.db.collection("Boards").add({ Name: this.boardName, Lists: [], Cards: [] });
+    this.db.collection("Boards").add({ Name: this.boardName, Lists: [], Cards: [], Members: [] });
   }
 
   resolveName(name) {
